8102c94ec0fdcb2bcd348840e1151e32b5ff874c
MiruCoffee <weihuasha1970@icloud.com>
2025-07-13 13:00:57 -0500

    firebase update

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,12 +1,8 @@
 // File: firebase-init.js
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// If using other Firebase services like Auth, Firestore, etc., import them here
-// e.g., import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyANBt_Iu5eow5eQ8racvqtXDbqV1cP8b6Y",
   authDomain: "miru-coffee.firebaseapp.com",
@@ -16,8 +12,6 @@ const firebaseConfig = {
   appId: "1:478375119822:web:b29ca2b1686b2ab59af99f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export app for use in other scripts if needed
+export const auth = getAuth(app);
 export default app;
