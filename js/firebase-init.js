ddb09b68c77878dafd0a342a8f65b898bab21bdf
MiruCoffee <weihuasha1970@icloud.com>
2025-07-13 12:11:54 -0500

    修改firebase js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyANBt_Iu5eow5eQ8racvqtXDbqV1cP8b6Y",
   authDomain: "miru-coffee.firebaseapp.com",
   projectId: "miru-coffee",
-  storageBucket: "miru-coffee.firebasestorage.app",
+  storageBucket: "miru-coffee.appspot.com",
   messagingSenderId: "478375119822",
   appId: "1:478375119822:web:b29ca2b1686b2ab59af99f"
 };
